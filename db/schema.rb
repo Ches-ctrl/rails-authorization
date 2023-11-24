83115c7c25b9799388d40e8e9872efa9970e3c0e
Charlie Cheesman <djnr.cheesman@gmail.com>
2023-11-24 09:18:37 +0000

    new rails project for Le Wagon

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_24_091556) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_24_091625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_091556) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "restaurants", "users"
 end
